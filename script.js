a153f6cae12bc4e655ad5536acef3fd25303d442
RumatoEstorski <73052701+RumatoEstorski@users.noreply.github.com>
2021-09-18 12:22:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let a = prompt("Enter number of cats");
+
+// while (i<a) {
+//   document.write("=^.^=");
+//   i++;
+// }
+// do
+// {
+// 	document.write("=^.^=");
+// 	i++;
+// }while(i<a)
+for (let i = 0; i < a; i++) {
+  document.write("=^.^=");
+}
